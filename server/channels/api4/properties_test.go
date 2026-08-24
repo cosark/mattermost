f2f019e8a6f2a198c6b11f130157d093e94cab5e
Miguel de la Cruz <miguel@ctrlz.es>
2026-08-24 19:21:00 +0200

    Minor fixes

--- server/channels/api4/properties_test.go
+++ server/channels/api4/properties_test.go
@@ -391,6 +391,7 @@ func TestCreatePropertyField(t *testing.T) {
 			_, resp, err := th.Client.CreatePropertyField(context.Background(), group.Name, "post", field)
 			require.Error(t, err)
 			CheckForbiddenStatus(t, resp)
+			CheckErrorID(t, err, "api.property_field.create.creator_cannot_edit.app_error")
 		})
 
 		t.Run("sysadmin and none are allowed on the values and options slots", func(t *testing.T) {
@@ -432,6 +433,7 @@ func TestCreatePropertyField(t *testing.T) {
 			_, resp, err := th.Client.CreatePropertyField(context.Background(), group.Name, "post", field)
 			require.Error(t, err)
 			CheckForbiddenStatus(t, resp)
+			CheckErrorID(t, err, "api.property_field.create.creator_cannot_edit.app_error")
 		})
 
 		t.Run("sysadmin submitting permission_field none is refused the same way", func(t *testing.T) {
@@ -444,6 +446,7 @@ func TestCreatePropertyField(t *testing.T) {
 			_, resp, err := th.SystemAdminClient.CreatePropertyField(context.Background(), group.Name, "post", field)
 			require.Error(t, err)
 			CheckForbiddenStatus(t, resp)
+			CheckErrorID(t, err, "api.property_field.create.creator_cannot_edit.app_error")
 		})
 	})
 
@@ -535,6 +538,7 @@ func TestCreatePropertyField(t *testing.T) {
 			_, resp, err := th.Client.CreatePropertyField(context.Background(), group.Name, "post", field)
 			require.Error(t, err)
 			CheckForbiddenStatus(t, resp)
+			CheckErrorID(t, err, "api.property_field.create.creator_cannot_edit.app_error")
 		})
 	})
 
